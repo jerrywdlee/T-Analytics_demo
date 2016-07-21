1ea7b0b5fe0444742e3ee40159f8862f9018aa5f
Jerry Lee <lzjjerrywoodylee@gmail.com>
2016-07-21 22:10:07 +0900

    画面微調整

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var breadcrumbs = require('express-breadcrumbs');
 
 var app = express();
 var port = 3033
 
 var index_config = {
+  user_name : 'Pirachu',
+  user_auth : '管理者',
+  user_company : 'TEMONA Inc.',
+  homepage_name : 'ホーム',
+  homepage_url : '/',
   search_box_placeholder : '商品・顧客',
   issuance_date : '1970-2016',
   right_holder : 'Jerry Lee',
@@ -14,17 +20,28 @@ var index_config = {
 //app.set('view engine', 'ejs');
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-//app.use(express.static( __dirname + '/public'));
-//app.use(express.static('public'));
-//app.use("/public", express.static(__dirname + '/public'));
 app.use(express.static('public'));
+app.use(breadcrumbs.init());
+// Set Breadcrumbs home information
+app.use('/',breadcrumbs.setHome({
+  name: index_config.homepage_name,
+  url: index_config.homepage_url
+}));
+
 var server = require('http').createServer(app)
 server.listen(process.env.PORT||port)
 
-app.get('/', function (req, res) {
+app.get('/',function (req, res) {
+  res.redirect('dashboard');
+})
+
+app.get('/dashboard', function (req, res) {
+  var page_title = 'ダッシュボード'
+  req.breadcrumbs(page_title);
   res.render('index2', {
     index_config,
-    page_title : 'T-Analytics Dashboard'
+    page_title : page_title,
+    breadcrumbs: req.breadcrumbs()
     //server_url: server_url
  });
 });
